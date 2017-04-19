2430c017c8ba4312367754d15bebe07003a4169f
JeeBak Kim <jeebak.kim@gmail.com>
2017-04-18 19:50:08 -0700

    Rename to cache_* functions, and simplify params

--- .lib/scripts/common.bash
+++ .lib/scripts/common.bash
@@ -2,31 +2,40 @@
 
 # http://stackoverflow.com/a/6110446
 set -e
+# shellcheck disable=SC2154
 trap 'prev_cmd=$this_cmd; this_cmd=$BASH_COMMAND' DEBUG
+# shellcheck disable=SC2154
 trap 'EC=$?; BAD_CMD="$prev_cmd"; [[ $EC -ne 0 ]] && echo "Exit: $EC, due to: $BAD_CMD"' EXIT
 
 CACHE_ROOT="$HOME/.cache/dotfiles"
 
-mkdir_p () {
+cache_mkdir_p () {
   for i in "$@"; do
-    [[ ! -d "$i"     ]] && mkdir -p "$i" || true
+   # shellcheck disable=SC2015
+    [[ ! -d "$CACHE_ROOT/$i" ]] && mkdir -p "$CACHE_ROOT/$i" || true
   done
 }
 
-verifylink () {
-  [[ "$(readlink "$1")" == "$2" ]]
+cache_mkdir_p "$CACHE_ROOT"
+
+# $1 is relative to $HOME
+# $2 is relative to $CACHE_ROOT
+cache_verifylink () {
+  [[ "$(readlink "$HOME/$1")" == "$CACHE_ROOT/$2" ]]
 }
 
-makelink () {
-  [[ ! -e "$1" ]] && ln -svf "$2" "$1" || true
+# $1 is relative to $HOME
+# $2 is relative to $CACHE_ROOT
+cache_makelink () {
+   # shellcheck disable=SC2015
+  [[ ! -e "$HOME/$1" ]] && ln -svf "$CACHE_ROOT/$2" "$HOME/$1" || true
 }
 
-shallow_clone () {
-  [[ ! -d "$2" ]] && git clone --depth 1 "$1" "$2" || true
+cache_shallow_clone () {
+   # shellcheck disable=SC2015
+  [[ ! -d "$CACHE_ROOT/$2" ]] && git clone --depth 1 "$1" "$CACHE_ROOT/$2" || true
 
-  pushd "$2" > /dev/null
+  pushd "$CACHE_ROOT/$2" > /dev/null
   git submodule init && git submodule update
   popd > /dev/null
 }
-
-mkdir_p "$CACHE_ROOT"
